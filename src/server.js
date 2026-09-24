86d8435bc28d55059904a49bbf4f86bfc94f0cd0
shlomofeit <11.5sfeit@gmail.com>
2026-09-24 09:29:31 +0300

    Added io.on with registerRoomHandler to server.js

--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ const app = express();
 const httpServer = http.createServer(app);
 const io = new Server(httpServer);
 
+io.on("connection", (socket) => {
+  registerRoomHandler(socket);
+});
+
 httpServer.listen(3001, () => {
   console.log("Listening on port 3001...");
 });
