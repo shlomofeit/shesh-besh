5145b747876deb31e290f8fbedf58ffd0615286d
shlomofeit <11.5sfeit@gmail.com>
2026-09-24 10:01:03 +0300

    Added try/catch to create room handler

--- src/socket/roomHandler.js
+++ src/socket/roomHandler.js
@@ -3,9 +3,18 @@ import { createRoom } from "../services/roomService.js";
 /** @param {import("socket.io").Socket} socket */
 export function registerRoomHandler(socket) {
   socket.on("room:create", async (data, callback) => {
-    const room = createRoom(socket.id, data.name);
-    await socket.join(room.id);
+    try {
+      const room = createRoom(socket.id, data.name);
+      await socket.join(room.id);
 
-    callback({ room });
+      callback({ room });
+    } catch (error) {
+      callback({
+        success: false,
+        error: {
+          message: error.message,
+        },
+      });
+    }
   });
 }
